891b443fff6e69ed5c33ec111a43e0a20fb4fa8e
TheAssassin <theassassin@assassinate-you.net>
2018-07-18 16:02:46 +0200

    32-bit builds, mk. 4

--- cmake/toolchains/i386-linux-gnu.cmake
+++ cmake/toolchains/i386-linux-gnu.cmake
@@ -12,11 +12,16 @@ set(CMAKE_SYSTEM_PROCESSOR i386 CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS "-m32" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "" FORCE)
 
+# CMAKE_SHARED_LINKER_FLAGS, CMAKE_STATIC_LINKER_FLAGS etc. must not be set, but CMAKE_EXE_LINKER_FLAGS is necessary
+set(CMAKE_EXE_LINKER_FLAGS "-m32" CACHE STRING "" FORCE)
+
+# host = target system
+# build = build system
+# both must be specified
+set(EXTRA_CONFIGURE_FLAGS "--host=i686-pc-linux-gnu" "--build=x86_64-pc-linux-gnu" CACHE STRING "" FORCE)
+
 # may help with some rare issues
 set(CMAKE_PREFIX_PATH /usr/lib/i386-linux-gnu CACHE STRING "" FORCE)
 
 # makes sure that at least on Ubuntu pkg-config will search for the :i386 packages
 set(ENV{PKG_CONFIG_PATH} /usr/lib/i386-linux-gnu/pkgconfig/ CACHE STRING "" FORCE)
-
-# make qtchooser find qt5
-set(ENV{QT_SELECT} qt5 CACHE STRING "" FORCE)
